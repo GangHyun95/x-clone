fffc6aac8e1204ce5b06cc2d33548b00da852074
허강현 <hgh6128@gmail.com>
2025-05-18 06:20:37 +0900

    게시글 전체, 팔로잉 피드, 좋아요한 게시글, 유저별 게시글 조회 기능 추가

--- backend/src/routes/post.route.ts
+++ backend/src/routes/post.route.ts
@@ -4,11 +4,19 @@ import {
     createPost,
     deletePost,
     editPost,
+    getAllPosts,
+    getFollowingPosts,
+    getLikedPosts,
+    getUserPosts,
     likeUnlikePost,
 } from '../controllers/post.controller.ts';
 
 const router = express.Router();
 
+router.get('/', getAllPosts);
+router.get('/following', getFollowingPosts);
+router.get('/:id/likes', getLikedPosts);
+router.get('/:nickname', getUserPosts);
 router.post('/', createPost);
 router.patch('/:id', editPost);
 router.delete('/:id', deletePost);
