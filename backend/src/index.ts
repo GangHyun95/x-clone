84d052dab800194fab9ecb99abc7798786677f28
허강현 <118712274+GangHyun95@users.noreply.github.com>
2025-05-20 10:48:04 +0900

    chore(backend): CORS 설정

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import authRoutes from './routes/auth.route.ts';
 import userRoutes from './routes/user.route.ts';
@@ -17,11 +18,17 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    })
+);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', protectRoute, userRoutes);
 app.use('/api/posts', protectRoute, postRoutes);
-app.use('/api/notifications',protectRoute, notificationRoutes);
+app.use('/api/notifications', protectRoute, notificationRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
