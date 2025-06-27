caf3422fe877f0b5c10a9cb146b32e6639f0d3db
허강현 <hgh6128@gmail.com>
2025-06-27 12:10:07 +0900

    feat: 모바일 Post 버튼 추가 및 daisyUI 모달 트랜지션 제거

--- frontend/src/components/mobile/BottomNav.tsx
+++ frontend/src/components/mobile/BottomNav.tsx
@@ -1,5 +1,6 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
+import { PostSvg } from '@/components/svgs';
 import { navItems } from '@/constants/navItems';
 import { useBottomBarFade } from '@/hooks/useBottomBarFade';
 import { getCurrentUser } from '@/store/authStore';
@@ -7,23 +8,31 @@ import { getCurrentUser } from '@/store/authStore';
 export default function BottomNav() {
     const { username } = getCurrentUser();
     const opacity = useBottomBarFade();
-
+    const navigate = useNavigate();
     return (
-        <nav
-            style={{ opacity }}
-            className='fixed bottom-0 left-0 right-0 flex justify-around items-center h-13 bg-white transition-opacity duration-200 xs:hidden'
-        >
-            {navItems.map(({ name, icon: Icon, path }) => {
-                const finalPath = name === 'profile' ? `/profile/${username}` : path;
-                return (
-                    <NavLink
-                        key={name}
-                        to={finalPath}
-                        className='flex flex-col items-center justify-center p-2'>
-                        {({ isActive }) => <Icon className='w-6 h-6' filled={isActive} />}
-                    </NavLink>
-                );
-            })}
-        </nav>
+        <div style={{ opacity }}>
+            <button
+                onClick={() => navigate('/post/new')}
+                className='btn btn-primary btn-circle fixed bottom-18 right-5 xs:hidden w-14 h-14'
+            >
+                <PostSvg className='fill-white w-6 h-6'/>
+            </button>
+
+            <nav
+                className='fixed bottom-0 left-0 right-0 flex justify-around items-center h-13 bg-white transition-opacity duration-200 xs:hidden'
+            >
+                {navItems.map(({ name, icon: Icon, path }) => {
+                    const finalPath = name === 'profile' ? `/profile/${username}` : path;
+                    return (
+                        <NavLink
+                            key={name}
+                            to={finalPath}
+                            className='flex flex-col items-center justify-center p-2'>
+                            {({ isActive }) => <Icon className='w-6 h-6' filled={isActive} />}
+                        </NavLink>
+                    );
+                })}
+            </nav>
+        </div>
     );
 }
