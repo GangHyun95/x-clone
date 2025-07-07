50fc90bd484a736a08675cf492c78f5c694c9710
허강현 <hgh6128@gmail.com>
2025-07-08 00:18:26 +0900

    fix: 라우팅 모달 시 스크롤 최상단 이동하는 버그 수정

--- frontend/src/components/common/ScrollToTop.tsx
+++ frontend/src/components/common/ScrollToTop.tsx
@@ -2,11 +2,13 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function ScrollToTop() {
-    const { pathname, search } = useLocation();
+    const location = useLocation();
+    const isModal = Boolean(location.state?.backgroundLocation);
 
     useEffect(() => {
+        if (isModal) return;
         window.scrollTo({ top: 0 });
-    }, [pathname, search]);
+    }, [location.pathname, location.search, isModal]);
 
     return null;
 }
